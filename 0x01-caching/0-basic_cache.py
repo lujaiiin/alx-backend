8aee0d1964a87fc4fab22a84ef4eef1de5868521
lujain <lujain.com>
2024-06-23 18:53:20 +0200

    lifo

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""modules"""
+""" BasicCache module
+"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache class"""
+    """ BasicCache inheretes from BaseCaching
+        and over rides
+      - get wich was not implemented
+      - put wich was not implemented
+    """
     def __init__(self):
-        """ Init"""
+        """ Initiliaze
+        """
         super().__init__()
 
     def put(self, key, item):
-        """put"""
+        """ Add an item in the cache
+        """
         if not key or not item:
             return
         self.cache_data.update({key: item})
 
     def get(self, key):
-        """get"""
+        """ Get an item by key
+        """
         return self.cache_data.get(key)
